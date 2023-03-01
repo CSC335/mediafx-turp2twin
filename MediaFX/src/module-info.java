46f1b83e1dbdc8eca0657dcffc023adf13ce5130
Nik Turpin <turp2twin@gmail.com>
2023-03-01 13:03:49 -0700

    Made small changes to PlayOneMP3.java

---
+++ MediaFX/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author frodo
+ *
+ */
+module MediaFX {
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.controls;
+	opens PlaySongDemo to javafx.graphics;
+}
